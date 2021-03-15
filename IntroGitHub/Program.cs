7f36d80ca304ee68f90cc47dc7a75bbc3c7061a8
unknown <00216920@uca.edu.sv>
2021-03-14 23:59:14 -0500

    initialized project

---
+++ IntroGitHub/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IntroGitHub
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.Write("Ingrese un valor para a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un valor para b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nSumando {a} + {b} igual: {AddNumber(a,b)} ");
+        }
+
+        public static int AddNumber(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
